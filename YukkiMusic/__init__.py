d35fb33ade30aef3f740f575a22470dfb2cde536
Mr-Sakil <142120431+Mr-Sakil@users.noreply.github.com>
2024-08-21 00:26:14 +0530

    Add files via upload

--- YukkiMusic/__init__.py
+++ YukkiMusic/__init__.py
@@ -1,32 +1,46 @@
+#
+# Copyright (C) 2024 by TheTeamVivek@Github, < https://github.com/TheTeamVivek >.
+#
+# This file is part of < https://github.com/TheTeamVivek/YukkiMusic > project,
+# and is released under the MIT License.
+# Please see < https://github.com/TheTeamVivek/YukkiMusic/blob/master/LICENSE >
+#
+# All rights reserved.
+
 from YukkiMusic.core.bot import YukkiBot
 from YukkiMusic.core.dir import dirr
 from YukkiMusic.core.git import git
 from YukkiMusic.core.userbot import Userbot
-from YukkiMusic.misc import dbb, heroku
-
-from SafoneAPI import SafoneAPI
+from YukkiMusic.misc import dbb, heroku, sudo
 from .logging import LOGGER
 
+# Directories
 dirr()
+
+# Check Git Updates
 git()
+
+# Initialize Memory DB
 dbb()
+
+# Heroku APP
 heroku()
 
+# Load Sudo Users from DB
+sudo()
+# Bot Client
 app = YukkiBot()
-userbot = Userbot()
-api = SafoneAPI()
 
+# Assistant Client
+userbot = Userbot()
 
 from .platforms import *
 
-Apple = AppleAPI()
-Carbon = CarbonAPI()
-SoundCloud = SoundAPI()
-Spotify = SpotifyAPI()
-Resso = RessoAPI()
-Telegram = TeleAPI()
 YouTube = YouTubeAPI()
-YTB = YTM()
+Carbon = CarbonAPI()
+Spotify = SpotifyAPI()
+Apple = AppleAPI()
+Resso = RessoAPI()
+SoundCloud = SoundAPI()
+Telegram = TeleAPI()
 HELPABLE = {}
-
-APP = "UMIKO_CHAT_BOT"  # connect music api key "Dont change it"
