c69a0d090d62fb6fa6932080de27864371ded954
Mr-Sakil <142120431+Mr-Sakil@users.noreply.github.com>
2024-08-21 00:25:52 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
-    return "Vip_Sakil_Bio"
+def hello():
+    return "Hi"
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=80)
